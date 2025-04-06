768cd71ad8f0bf828bc6c9af69c11102cefa66e4
karims7 <karims7@my.yorku.ca>
2025-04-06 02:32:41 -0400

    initialized dataset creation file

--- create_ds.py
+++ create_ds.py
@@ -1,3 +1,12 @@
 import os
+import pandas as pd
 import shutil
 import random
+from pathlib import Path
+from tqdm import tqdm
+
+def create_dataset_structure(): 
+    pass
+
+def main(): 
+    pass
